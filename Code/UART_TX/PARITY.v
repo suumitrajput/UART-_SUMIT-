d4914c02c2ed2162224080cc7f6b7f75bbf8b81d
suumitrajput <suumitrajput22singh@gmail.com>
2025-07-28 15:04:44 +0530

    Create PARITY.v

---
+++ Code/UART_TX/PARITY.v
@@ -0,0 +1,25 @@
+module Parity(
+  input wire         reset_n,     //  Active low reset.
+  input wire  [7:0]  data_in,     //  The data input from the InReg unit.
+  input wire  [1:0]  parity_type, //  Parity type agreed upon by the Tx and Rx units.
+
+  output reg         parity_bit   //  The parity bit output for the frame.
+);
+
+//  Encoding for the parity types
+localparam ODD        = 2'b01,
+           Even       = 2'b10;
+
+always @(*)
+begin
+  if (!reset_n) parity_bit = 1'b1;
+  else
+  begin
+    case (parity_type)
+    ODD:     parity_bit = (^data_in)? 1'b0 : 1'b1;
+    Even:    parity_bit = (^data_in)? 1'b1 : 1'b0; 
+    default: parity_bit = 1'b1;
+    endcase
+  end
+end
+endmodule
